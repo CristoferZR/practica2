62df5a3d43737393a636ad028f0cb701bc063272
Cristofer Zamora <104957113+CristoferZR@users.noreply.github.com>
2023-02-28 19:47:48 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function cambiarTexto(id) {
+
+    var cambio = document.getElementById(id);
+
+    cambio.textContent="Agregado al Carrito";
+    cambio.style.backgroundColor="red";
+    
+}
+
+
